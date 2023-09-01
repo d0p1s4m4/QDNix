42c25d4c08a18759d13abf15d95e903ab4a092ed
d0p1s4m4 <d0p1s4m4@users.noreply.github.com>
2023-09-01 11:33:39 +0000

    deploy: 5f886bee248f8b5dd50e8c6514ff85bb2a8619bc

---
+++ static/js/quote.js
@@ -0,0 +1,15 @@
+const quotes = [
+	["UNIX is very simple, it just needs a genius to understand its simplicity.", "Dennis Ritchie"],
+	["UNIX was not designed to stop its users from doing stupid things, as that would also stop them from doing clever things.", "Doug Gwyn"],
+	["Pretty much everything on the web uses those two things: C and UNIX.", "Dennis Ritchie"]
+];
+
+const selected = quotes[Math.floor(Math.random() * quotes.length)];
+
+const quoteElement = document.getElementById("quote-text");
+const authorElement = document.getElementById("quote-author");
+
+if (authorElement !== null && quoteElement !== null)
+{
+	[quoteElement.innerText, authorElement.innerText] = selected;
+}
